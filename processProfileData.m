c8a2256485ad6d0aada5e91f18ec6f266209a3e0
Jessy Barrette <jessy.barrette@hakai.org>
2021-01-04 12:52:46 -0800

    Make processProfileData  more adaptable to different sensors.

--- processProfileData.m
+++ processProfileData.m
@@ -1,13 +1,7 @@
 function rsk = processProfileData(inFile)
 
 
-%% Default values
-% Since we don't know the location exactely we'll use some ramdom 
-% location south of the Caspian Sea
-metadata.latitude = 37.580321;
-metadata.longitude= 51.636416;
-
-%% Load File
+%% Read Raw File Path
 [path, file, ext] = fileparts(inFile);
 
 %% Read Data From Different Format
@@ -37,25 +31,28 @@ rsk(end) = RSKderivedepth(rsk(end),'latitude',metadata.latitude);
 rsk(end) = RSKderivevelocity(rsk(end));
 
 % Smooth Data
-rsk(end) = RSKsmooth(rsk,{'Conductivity','Temperature'},'windowLength',3);
+rsk(end) = RSKsmooth(rsk,'channel',{'Conductivity','Temperature'},'windowLength',process.smoothCT.windowLength);
 
-% Align Data (just an example with Oxygen)
-rsk(end) = RSKalignchannel(rsk,'Dissolved Oxygen',3,'lagunits','seconds');
-rsk(end) = RSKalignchannel(rsk,'Temperature',1/3,'lagunits','seconds');
+% Align CT and DO data
+rsk(end) = RSKalignchannel(rsk,'channel','Temperature',...
+    'lag',process.alignCT.lag,'lagunits',process.alignCT.lagunits);
+rsk(end) = RSKalignchannel(rsk,'channel','Dissolved Oxygen',...
+    'lag',process.alignDO.lag,'lagunits',process.alignDO.lagunits); 
 
 % Derive Variables
-%rsk(end) = RSKderivesalinity(rsk(end)); # The Conductivity data isn't in mS/cm, I won't do any recompute it since it will overwrite the value
+rsk(end) = RSKderivesalinity(rsk(end)); 
 
 % Split up/down casts
 rsk(end) = RSKtimeseries2profiles(rsk(end));
 
-% Loop average
-rsk(end) = RSKremoveloops(rsk(end),'threshold',.25);
+% Loop average (remove loop the in profile)
+rsk(end) = RSKremoveloops(rsk(end)); %Default threshold value is 0.25m/s
 
 % Bin average
-rsk(end) = RSKbinaverage(rsk(end));
+rsk(end) = RSKbinaverage(rsk(end)); % by default it creates 1m bins
 
 %Write processed data!
 RSK2ODV(rsk(end)); % ODV is a good format to use
 RSK2CSV(rsk(end)); % Could be useful to
 RSKplotprofiles(rsk(end)); % Let's show the resulting plots
+
